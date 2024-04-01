da7bc9f26e6c7cf4425ca8d253615843b959825c
Hariom Singh Thakur <hariomthakur638@gmail.com>
2024-04-01 14:35:41 +0530

    files saved

---
+++ file.java
@@ -0,0 +1,9 @@
+public class file{
+
+    public static void main(String[] args){
+
+        System.out.println("hello world");
+        System.out.println("hello hariom");
+        System.out.println("hello coders");
+    }
+}
